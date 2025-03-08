e8335156196869a20de61ed2ad5e84deed43a537
TheHydraSlayer <alexandersmccollum@gmail.com>
2025-03-08 14:21:35 -0600

    Added list items functionality

--- qless/src/components/kitchen/ListOrders.tsx
+++ qless/src/components/kitchen/ListOrders.tsx
@@ -1,11 +1,57 @@
 import '../../styles/kitchen/cookDashboard.css';
 
+//temporary objects for testing 
+const orders = [{
+    orderId: 0,
+    timePlaced: new Date(2025, 2, 8, 10, 0, 0, 0),
+    orderStatus: "In Progress"
+}, {
+    orderId: 1,
+    timePlaced: new Date(2025, 2, 8, 10, 5, 0, 0),
+    orderStatus: "In Progress"
+}]
 
-
-export default function ListOrders(){
-    //get the active orders from the db and store in array
-    
-    //map each active order to a component
-    
+//gets active orders from db and stores in array
+function getOrders() {
+    const openOrders= orders.filter(order => order.orderStatus!=="Finished")
+    return openOrders;
 }
 
+export default function ListOrders({ setIsShowing: setIsShowing, setOrderNum: setOrderNum }:
+    { setIsShowing: React.Dispatch<React.SetStateAction<string>>; setOrderNum: React.Dispatch<React.SetStateAction<number>> }) {
+    const openOrders = getOrders();
+
+    //for when you click the "veiw details" button
+    function handleDetailsClick(orderId: number) {
+        const click = () => {
+            setIsShowing("details");
+            setOrderNum(orderId);
+        }
+        return click;
+    }
+    
+    //for when you click the "finish" button
+    function handleFinishClick(orderId: number){
+        const click = () => {
+            setIsShowing("finish");
+            setOrderNum(orderId);
+        }
+        return click;
+    }
+
+    //map each active order
+    const listItems = openOrders.map(order =>
+        <li key={order.orderId}>
+            <ul className='orderList'>
+                <li>Order Number: {order.orderId}</li>
+                <li>Time Submitted: {order.timePlaced.toLocaleTimeString()}</li>
+                <li>Status: {order.orderStatus}</li>
+            </ul>
+            <button className='listButton' onClick={handleDetailsClick(order.orderId)}>View Details</button>
+            <button className='listButton' onClick={handleFinishClick(order.orderId)}>Finish</button>
+        </li>
+    );
+
+    //return list
+    return <ol>{listItems}</ol>
+}
--- qless/src/pages/kitchen/CookDashboard.tsx
+++ qless/src/pages/kitchen/CookDashboard.tsx
@@ -1,12 +1,37 @@
+import ListOrders from '../../components/kitchen/ListOrders';
 import '../../styles/kitchen/cookDashboard.css';
+import { useState } from 'react';
 
-function CookDashboard(){
+export default function CookDashboard() {
+    const [isShowing, setIsShowing] = useState("list");
+    const [orderNum, setOrderNum] = useState(0);
 
-    return(
+    return (
         <div className='pageContainer'>
-            
+            {isShowing === "list" && <ListOrders setIsShowing={setIsShowing} setOrderNum={setOrderNum} />}
+            {isShowing === "details" && <DetailsButton setIsShowing={setIsShowing} orderNum={orderNum} />}
+            {isShowing === "finish" && <FinishButton setIsShowing={setIsShowing} orderNum={orderNum}/>}
         </div>
     )
 }
 
-export default CookDashboard;
+function DetailsButton({ setIsShowing, orderNum }:
+    { setIsShowing: React.Dispatch<React.SetStateAction<string>>; orderNum: number }) {
+    return (
+        <>
+            <h1>Details for: {orderNum}</h1>
+            <button onClick={() => setIsShowing("list")}>Back</button>
+        </>
+    );
+}
+
+function FinishButton({ setIsShowing, orderNum }:
+    { setIsShowing: React.Dispatch<React.SetStateAction<string>>; orderNum: number }) {
+    return(
+        <>
+            <h1>Finish for: {orderNum}</h1>
+            <button onClick={() => setIsShowing("list")}>Back</button>
+        </>
+    );
+}
+
